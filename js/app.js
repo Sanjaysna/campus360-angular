5590d067c3a8b138d0ce9a1081191ad66528a34d
Sanjaysna <sanjaysanthosh411@gmail.com>
2026-07-23 08:41:29 +0530

    ng-binding

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
-var app = angular.module('Campus360', []);
+var app = angular.module("Campus360", []);
 
-app.controller('dashboardcontroller', function ($scope) {
+app.controller("dashboardcontroller", function ($scope) {
+
     console.log("Dashboard controller is loaded");
+
     $scope.collegename = "FISAT";
     $scope.totalstudents = 1200;
     $scope.totalfaculty = 100;
 
+    $scope.showAddStudentForm = false;
+    $scope.showdashboard = true;
+    $scope.fees = 23000;
+    $scope.today = new Date();
+
+
+    $scope.departments=["CSE","MCA","MBA","M-com","EEE"];
+    $scope.selectedDepartment = "CSE";
+
+    $scope.isdisabled = true;
+
+    $scope.allowAdmission = function () {
+        $scope.isdisabled = false;
+    };
+
+    $scope.isReadOnly=true;
+    $scope.toggleReadOnly = function () {
+        $scope.isReadOnly = !$scope.isReadOnly;
+    };
+
+
+
+    $scope.showAddStudentForm = false;
+
+    $scope.addStudent = function () {
+        $scope.totalstudents++;
+    };
 
     $scope.students = [
         { name: "Varsha" },
@@ -13,11 +42,5 @@ app.controller('dashboardcontroller', function ($scope) {
         { name: "Alice" },
         { name: "Bob" }
     ];
-
-    $scope.addStudent = function () {
-        $scope.totalstudents++;
-        $scope.showAddStudentForm = false;
-        $scope.showdashboard = true;
-    }
 
 });
